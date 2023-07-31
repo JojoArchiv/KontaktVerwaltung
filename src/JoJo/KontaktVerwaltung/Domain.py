b72a457ee54564143fbf5ec2cb7ea68a19f0528d
JojoArchiv <johanna.wolfstieg@posteo.de>
2023-07-31 11:38:42 +0200

    Domain-Modul anlegen.

---
+++ src/JoJo/KontaktVerwaltung/Domain.py
@@ -0,0 +1,17 @@
+'''
+Created on 31.07.2023
+
+@author: Jojo
+'''
+
+class Kontakt(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
